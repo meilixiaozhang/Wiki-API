a627958027deab4d1211f38c7cd378124677ca15
meilixiaozhang <redpangci@gmail.com>
2020-04-13 20:29:49 +1000

    PUT to update a whole article

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 const articleSchema = {
   title: String,
@@ -22,53 +29,72 @@ const Article = mongoose.model("Article", articleSchema);
 
 //////////// Requests targetting on all articles ////////////
 app.route("/articles")
-// GET all articles
-.get(function(req,res){
-  Article.find(function(err,foundArticles){
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+  // GET all articles
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    })
   })
-})
-// POST a new article
-.post(function(req,res){
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save(function(err){
-    if (err) {
-      res.send(err);
-    } else {
-      res.send("Sucessfully added a new article.")
-    }
-  });
-})
-// DELETE all articles
-.delete(function(req, res){
-  Article.deleteMany(function(err){
-    if (err) {
-      res.send(err);
-    } else {
-      res.send("Successfully deleted all the articles.")
-    }
+  // POST a new article
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Sucessfully added a new article.")
+      }
+    });
   })
-});
+  // DELETE all articles
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully deleted all the articles.")
+      }
+    })
+  });
 
 //////////// Requests targetting on single article ///////////////
 app.route("/articles/:articleTitle")
-// GET one single article
-.get(function(req,res){
-  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No article found.");
-    }
+  // GET one single article
+  .get(function(req, res) {
+    Article.findOne({
+      title: req.params.articleTitle
+    }, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No article found.");
+      }
+    })
   })
-});
+  // .update() is deprecated instead use .updateOne() .replaceOne()
+  .put(function(req, res) {
+    Article.updateOne({
+      title: req.params.articleTitle
+    }, {
+      title: req.body.title,
+      content: req.body.content
+    }, function(err) {
+      if (!err) {
+        res.send("Successfully updated article")
+      } else {
+        res.send(err)
+      }
+    })
+  })
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
