ea131d6272a2ec5e5eb6aa42c3242ac21efd1ec3
meilixiaozhang <redpangci@gmail.com>
2020-04-13 18:47:13 +1000

    use express to chain routes

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// GET - fetch all articles
-app.get("/articles", function(req,res){
+app.route("/articles")
+// GET all articles
+.get(function(req,res){
   Article.find(function(err,foundArticles){
     if (!err) {
       res.send(foundArticles);
@@ -30,9 +31,8 @@ app.get("/articles", function(req,res){
     }
   })
 })
-
-// POST - create a new article
-app.post("/articles", function(req,res){
+// POST a new article
+.post(function(req,res){
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
@@ -46,8 +46,7 @@ app.post("/articles", function(req,res){
   });
 })
 
-// DELETE - delete all articles
-app.delete("/articles", function(req, res){
+.delete(function(req, res){
   Article.deleteMany(function(err){
     if (err) {
       res.send(err);
@@ -55,7 +54,7 @@ app.delete("/articles", function(req, res){
       res.send("Successfully deleted all the articles.")
     }
   })
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
